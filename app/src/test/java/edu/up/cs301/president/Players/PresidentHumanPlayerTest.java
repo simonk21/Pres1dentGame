0f3c4e07559d36f91350e4f54b8e95df9c8fc06b
Kamalei Simon <simonk21@up.edu>
2019-04-23 20:02:32 -0700

    JUnit Testing: - PlayerTracker

---
+++ app/src/test/java/edu/up/cs301/president/Players/PresidentHumanPlayerTest.java
@@ -0,0 +1,22 @@
+package edu.up.cs301.president.Players;
+
+import org.junit.Test;
+
+public class PresidentHumanPlayerTest {
+
+    @Test
+    public void getTopView() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+
+    @Test
+    public void receiveInfo() {
+    }
+
+    @Test
+    public void setAsGui() {
+    }
+}
---
+++ app/src/test/java/edu/up/cs301/president/PresidentLocalGameTest.java
@@ -0,0 +1,38 @@
+package edu.up.cs301.president;
+
+import org.junit.Test;
+
+public class PresidentLocalGameTest {
+
+    @Test
+    public void sendUpdatedStateTo() {
+    }
+
+    @Test
+    public void canMove() {
+    }
+
+    @Test
+    public void checkIfGameOver() {
+    }
+
+    @Test
+    public void makeMove() {
+    }
+
+    @Test
+    public void play() {
+    }
+
+    @Test
+    public void trade() {
+    }
+
+    @Test
+    public void getMax() {
+    }
+
+    @Test
+    public void removeTrade() {
+    }
+}
